ef4c49869efdb988aed839faf7b8167134b4a3df
Guillermo <guillermofabrod@gmial.com>
2023-12-19 23:57:21 +0100

    v

---
+++ src/Entity/Idiomas.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\IdiomasRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: IdiomasRepository::class)]
+class Idiomas
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nombre = null;
+
+    #[ORM\OneToMany(mappedBy: 'idioma', targetEntity: Producto::class)]
+    private Collection $producto;
+
+    public function __construct()
+    {
+        $this->producto = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): static
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Producto>
+     */
+    public function getProducto(): Collection
+    {
+        return $this->producto;
+    }
+
+    public function addProducto(Producto $producto): static
+    {
+        if (!$this->producto->contains($producto)) {
+            $this->producto->add($producto);
+            $producto->setIdioma($this);
+        }
+
+        return $this;
+    }
+
+    public function removeProducto(Producto $producto): static
+    {
+        if ($this->producto->removeElement($producto)) {
+            // set the owning side to null (unless already changed)
+            if ($producto->getIdioma() === $this) {
+                $producto->setIdioma(null);
+            }
+        }
+
+        return $this;
+    }
+}
